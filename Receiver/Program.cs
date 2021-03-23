07af73d816d6d6a21f03d2680460e146b7b69967
Dionis Oliveira <dionisoliveira@users.noreply.github.com>
2021-03-23 17:59:46 -0300

    Add files via upload
    Add project sample send message and receiver message for RabbitMQ

---
+++ Receiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            //Select que
+            channel.QueueDeclare(queue: "samplefanout",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                 channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "samplefanout",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            
+
+
+            // Definição de fila.
+            // queue: nome 
+            channel.QueueDeclare(queue: "hello", //name bindingin to pool of message.
+                                 durable: true,
+                                 exclusive: false,//this queue is consuming in excluise worker? 
+                                 autoDelete: false,
+                                 arguments: null);
+
+            string message = "New messagem for rabbitmq";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            //add new properties in configuration queue message
+
+
+
+             /*With props
+            //IBasicProperties props = channel.CreateBasicProperties(); 
+           // props.ContentType = "text/json"; // Adiciona o tipo de dado
+
+            // Adiciona o tipo de persistência do objeto. 
+            // 1 for "transient" / 2 for "persistent". 
+           //   props.DeliveryMode = 2; 
+
+
+            channel.BasicPublish( "amq.fanout",//Name exchange you send thee message
+                                  "0001",//route queue
+                                 props,
+                                  body);
+                                  */
+// Basic Send messag
+                                     channel.BasicPublish(exchange: "",
+                                 routingKey: "samplename",
+                                 basicProperties: null,
+                                 body: body);
+                                 
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
+
